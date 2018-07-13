07a6a165fd9e80a819b4da2fc89a92f3752be640
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-13 18:14:11 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -11,8 +11,8 @@ class Detention
     @age
   end
 end
-maria= Detention.new("maria",15)
-rudy= Detention.new("rudy",14)
+maria= Detention.new
+rudy= Detention.new
 
 puts maria.name
 puts maria.age
