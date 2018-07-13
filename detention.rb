f9a2024436d9861e25e19e063040f2ff820d13cb
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-13 18:42:58 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,7 +1,7 @@
 #Code your detention class here
 class Detention 
   
-  attr_accessor :location, :time, :grumpy_teacher, :activity
+  attr_accessor :location, :time, :grumpy_teacher, :activity, :students
   
   
   def initialize
