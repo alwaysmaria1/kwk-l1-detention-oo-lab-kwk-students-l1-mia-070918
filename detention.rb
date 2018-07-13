0cc3adf814f9957daa9fc021d0b38b0ea3d82b0c
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-13 18:19:08 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,21 +1,12 @@
 #Code your detention class here
 class Detention 
   def initialize
-    # (name,age)
-    @name=name
-    @age=age
-  end
-  def name
-    @name
-  end
-  def age
-    @age
   end
 end
 maria= Detention.new
-# ("maria",15)
+
 rudy= Detention.new
-# ("rudy",14)
+
 
 puts maria.name
 puts maria.age
