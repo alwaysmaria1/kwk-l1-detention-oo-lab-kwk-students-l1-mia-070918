a161261fa668fc430db7b765f9466c0b5a5972b1
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-13 18:29:26 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,14 +1,19 @@
 #Code your detention class here
 class Detention 
+  attr_accessor :location 
+  
   def initialize
     @location= location
   end
-  def location=(new_location)
-    @location=new_location
-  end
-  def location
-    @location
-  end
+  
+  # def location=(new_location)
+  #   @location=new_location
+  # end
+  
+  # def location
+  #   @location
+  # end
+  
 end 
 maria= Detention.new
 
