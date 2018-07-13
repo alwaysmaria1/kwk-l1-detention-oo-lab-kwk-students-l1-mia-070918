8d0964abc9229429485edc02973a995c906e2a60
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-13 18:11:24 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,1 +1,19 @@
 #Code your detention class here
+class Detention 
+  def initialize
+    @name=name
+    @age=age
+  end
+  def name
+    @name
+  end
+  def age
+    @age
+  end
+end
+maria= Detention.new 
+rudy= Detention.new 
+puts maria.name
+puts maria.age
+puts rudy.name
+puts rudy.age
